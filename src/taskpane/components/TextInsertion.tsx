ff81d95d9f73dccf7deac45503bcbd18b1052dae
Sandro Forster <s.forster@opentalk.eu>
2025-04-16 12:03:54 +0200

    fix(ci): cover all files when formatting/checking with prettier

--- src/taskpane/components/TextInsertion.tsx
+++ src/taskpane/components/TextInsertion.tsx
@@ -43,7 +43,11 @@ const TextInsertion: React.FC<TextInsertionProps> = (props: TextInsertionProps) 
 
   return (
     <div className={styles.textPromptAndInsertion}>
-      <Field className={styles.textAreaField} size="large" label="Enter text to be inserted into the document.">
+      <Field
+        className={styles.textAreaField}
+        size="large"
+        label="Enter text to be inserted into the document."
+      >
         <Textarea size="large" value={text} onChange={handleTextChange} />
       </Field>
       <Field className={styles.instructions}>Click the button to insert text.</Field>
