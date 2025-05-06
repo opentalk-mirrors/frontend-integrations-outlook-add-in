8e1e4eb930616acf1c039bc6d0dde07c56e98da7
Sandro Forster <s.forster@opentalk.eu>
2025-05-06 12:56:32 +0200

    ci: configure deployment url in manifest based on environment variable

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -4,9 +4,8 @@
 #
 # SPDX-License-Identifier: EUPL-1.2
 
-# Creates a config.json file with a config object. Populates the values of the
+# Create a config.json file with a config object. Populates the values of the
 # config with environment variables that are present in the container.
-
 HTML_ROOT=/usr/share/nginx/html
 cat >$HTML_ROOT/config.json << EOF
 {
@@ -14,3 +13,9 @@ cat >$HTML_ROOT/config.json << EOF
   "opentalkOutlookHostUrl": "${OPENTALK_OUTLOOK_HOST_URL}"
 }
 EOF
+
+# Allow configuring the deployment url of the addin using an environment variable
+# Escape all forward slashes by adding a backward slash in front of them
+url=$(echo "$OPENTALK_OUTLOOK_URL" | sed 's/\//\\\//g')
+# Replace the development url with the deployment url in the manifest
+sed --in-place "s/https:\/\/localhost:3000/$url/g" "$HTML_ROOT"/manifest.xml
