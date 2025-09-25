bc26b2a3a25dd4f54c7565ec3321c9c65bfd2f8c
Oleksii Sukhodolskyi <o.sukhodolskyi@opentalk.eu>
2025-09-25 13:40:01 +0200

    fix: missing call-in translation

--- src/components/pages/EventBody/PhoneParticipation.tsx
+++ src/components/pages/EventBody/PhoneParticipation.tsx
@@ -3,23 +3,22 @@ import { FC } from "react";
 import { CallInInfo } from "../../../api/types/events";
 import { Caption } from "./Caption";
 import { Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export const PhoneParticipation: FC<CallInInfo> = ({ tel, id, password }) => {
+  const { t } = useTranslation();
   return (
     <Box style={{ margin: "20px 0" }}>
-      <Caption>Participate via Phone</Caption>
-      <Typography component="p">You can use your phone to participate in this meeting.</Typography>
-      <Typography component="p">
-        Just dial in the following number and enter the given conference ID and conference PIN, or
-        click the Quick-Dial number on your phone.
-      </Typography>
-      <strong>Number:</strong> {tel}
+      <Caption>{t("call-in-section-title", { ns: "invitation" })}</Caption>
+      <Typography component="p">{t("call-in-paragraph-title", { ns: "invitation" })}</Typography>
+      <Typography component="p">{t("call-in-paragraph-content", { ns: "invitation" })}</Typography>
+      <strong>{t("call-in-number")}:</strong> {tel}
       <br />
-      <strong>Conference ID:</strong> {id}
+      <strong>{t("call-in-conference-id")}:</strong> {id}
       <br />
-      <strong>Conference PIN:</strong> {password}
+      <strong>{t("call-in-conference-pin")}:</strong> {password}
       <br />
-      <strong>Quick-Dial:</strong> {tel},,{id},,{password}
+      <strong>{t("call-in-quick-dial")}:</strong> {tel},,{id},,{password}
       <br />
     </Box>
   );
